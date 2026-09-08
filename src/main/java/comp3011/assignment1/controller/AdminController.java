3ac4fd312daf83dcf3fc5286ed84b602300d32e7
Xuan-Tung-Nguyen <xuantung.nguyen01@student.adelaide.edu.au>
2026-09-08 12:45:39 +0930

    Add model, controller, service, and testing page

---
+++ src/main/java/comp3011/assignment1/controller/AdminController.java
@@ -0,0 +1,30 @@
+package comp3011.assignment1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+    private final Instant serverStart = Instant.now();
+
+    @GetMapping("/uptime")
+    public UptimeResponse getUptime() {
+        Instant now = Instant.now();
+        double seconds = Duration.between(serverStart, now).toMillis() / 1000.0;
+        return new UptimeResponse(serverStart.toString(), now.toString(), seconds);
+    }
+
+	public record UptimeResponse(
+	    String utcServerStart,
+	    String utcNow,
+	    double serverUptimeSeconds
+	) {}
+}
