4c335e020a246703b5fbdfa72e062d67f0445d64
Xuan-Tung-Nguyen <xuantung.nguyen01@student.adelaide.edu.au>
2026-09-09 20:40:05 +0930

    Add AtomicBoolean shuttingDown to prevent concurrent requests

--- src/main/java/comp3011/assignment1/controller/AdminController.java
+++ src/main/java/comp3011/assignment1/controller/AdminController.java
@@ -5,9 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import comp3011.assignment1.dto.ErrorResponse;
 import comp3011.assignment1.dto.UptimeResponse;
+import comp3011.assignment1.dto.ShutDownResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
 
 @RestController
@@ -15,11 +22,42 @@ import comp3011.assignment1.dto.UptimeResponse;
 public class AdminController {
 
     private final Instant serverStart = Instant.now();
-
+    private final AtomicBoolean shuttingDown = new AtomicBoolean(false); 
+    //prevent concurrent shutdown requests
+    
     @GetMapping("/uptime")
     public UptimeResponse getUptime() {
         Instant now = Instant.now();
         double seconds = Duration.between(serverStart, now).toMillis() / 1000.0;
         return new UptimeResponse(serverStart.toString(), now.toString(), seconds);
+        
+    }
+    
+    @PostMapping("/shutdown")
+    public ResponseEntity<?> shutdown(
+            HttpServletRequest request) {
+
+        if (!shuttingDown.compareAndSet(false, true)) {
+
+            ErrorResponse conflict =
+                    new ErrorResponse(
+                            Instant.now().toString(),
+                            409,
+                            "Conflict",
+                            "Graceful shutdown is already in progress.",
+                            request.getRequestURI()
+                    );
+
+            return ResponseEntity.status(409).body(conflict);
+        }
+        
+        return ResponseEntity
+                .accepted()
+                .body(
+                    new ShutDownResponse(
+                        "Graceful shutdown requested."
+                    )
+                );
     }
 }
+
