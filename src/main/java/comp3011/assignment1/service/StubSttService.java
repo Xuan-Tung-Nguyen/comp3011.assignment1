bdf62ad438a1a341cbecc668970d3bc6d4215209
Xuan-Tung-Nguyen <xuantung.nguyen01@student.adelaide.edu.au>
2026-09-12 20:06:32 +0930

    Adjust StubSttService & add fixed token counts

--- src/main/java/comp3011/assignment1/service/StubSttService.java
+++ src/main/java/comp3011/assignment1/service/StubSttService.java
@@ -1,5 +1,17 @@
 package comp3011.assignment1.service;
 
-public class StubSttService {
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
+import comp3011.assignment1.dto.TranscriptionResult;
+import reactor.core.publisher.Mono;
+
+@Service
+@Profile("stub")
+public class StubSttService implements SttService {
+    @Override
+    public Mono<TranscriptionResult> transcribe(byte[] audioBytes, String contentType) {
+        return Mono.just(new TranscriptionResult(
+            "stub transcript for " + audioBytes.length + " bytes", 1, 1));
+    }
 }
